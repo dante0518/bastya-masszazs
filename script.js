94fbe2b5523f88892cc94960d0d44efd8dcebd03
dante0518 <90151246+dante0518@users.noreply.github.com>
2024-08-06 22:17:03 +0200

    first-commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const bookNowbtn = document.getElementById('bookNow');
+const bookNowbtn1 = document.getElementById('bookNowBtn');
+
+bookNowbtn.addEventListener('click', function(){
+    window.location.href = 'booking.html'
+})
+bookNowbtn1.addEventListener('click', function(){
+    window.location.href = 'booking.html'
+})
